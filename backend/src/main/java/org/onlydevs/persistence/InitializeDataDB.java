410269717be6fee15d5b9c2f005f305335b6736b
Mike <mw-3@hotmail.nl>
2023-01-17 15:12:14 +0100

    rest of initiliaze db

--- backend/src/main/java/org/onlydevs/persistence/InitializeDataDB.java
+++ backend/src/main/java/org/onlydevs/persistence/InitializeDataDB.java
@@ -1,2 +1,46 @@
-package org.onlydevs.persistence;public class InitializeDataDB {
+package org.onlydevs.persistence;
+
+import lombok.AllArgsConstructor;
+import org.onlydevs.business.impl.CreateAccount;
+import org.onlydevs.business.impl.CreateAppointment;
+import org.onlydevs.domain.User;
+import org.onlydevs.persistence.entity.RoleEnum;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+
+@Component
+@AllArgsConstructor
+public class InitializeDataDB implements ApplicationRunner {
+    private final CreateAccount createAccount;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+
+    public Long registerSecretaryAccount()
+    {
+        User user = User.builder()
+                .email("secretary@gmail.com")
+                .password("secretary")
+                .roles(List.of(RoleEnum.SECRETARY.toString()))
+                .build();
+        return createAccount.register(user);
+    }
+
+    public Long registerAdminAccount()
+    {
+        User user = User.builder()
+                .email("admin@gmail.com")
+                .password("secretary")
+                .roles(List.of(RoleEnum.ADMIN.toString()))
+                .build();
+        return createAccount.register(user);
+    }
 }
+
